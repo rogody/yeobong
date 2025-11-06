bc683118c9860f6ccdb91eb19cffd273789d2d42
rogody <roahtgody@gmail.com>
2025-11-06 22:05:27 +0900

    initial commit

---
+++ chatbot/infra/llm_model.py
@@ -0,0 +1,42 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+class LLModel:
+    def __init__(self, model_name: str = "Qwen/Qwen3-4B-Instruct-2507"):
+        # load the tokenizer and the model
+        print("Loading model:", model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            torch_dtype="auto",
+            device_map="auto"
+        )
+        print("Model loaded successfully.")
+
+    def generate(self, prompt: str, max_new_tokens: int = 16384) -> str:
+        # prepare the model input
+        messages = [
+            {"role": "user", "content": prompt}
+        ]
+        text = self.tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True,
+        )
+        model_inputs = self.tokenizer([text], return_tensors="pt").to(self.model.device)
+
+        # conduct text completion
+        generated_ids = self.model.generate(
+            **model_inputs,
+            max_new_tokens=max_new_tokens
+        )
+        output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
+
+        content = self.tokenizer.decode(output_ids, skip_special_tokens=True)
+        return content
+    
+# Example usage:
+# model_name = "Qwen/Qwen3-4B-Instruct-2507"
+# llm_model = LLMModel(model_name)
+# prompt = "Give me a short introduction to large language model."
+# content = llm_model.generate_text(prompt)
+# print("content:", content)
---
+++ main.py
@@ -0,0 +1,18 @@
+from chatbot.infra.llm_model import LLModel
+from chatbot.ui.chatui import ChatUI, SimpleLLMBackend
+
+
+def main():
+    # 1) LLM 로드 (조금 시간 걸릴 수 있음)
+    llm_client = LLModel()
+
+    # 2) 간단 백엔드 생성
+    backend = SimpleLLMBackend(llm_client)
+
+    # 3) UI 실행
+    ui = ChatUI(backend)
+    ui.run()
+
+
+if __name__ == "__main__":
+    main()
