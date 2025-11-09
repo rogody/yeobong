494ef43373ed39d300d2b1005ed4bcd350e9e565
rogody <roahtgody@gmail.com>
2025-11-10 00:34:34 +0900

    구조?

---
+++ chatbot/domain/Types.py
@@ -0,0 +1,11 @@
+'''
+도메인에서 사용하는 자료형들을 정의
+example
+class Pad
+    pleasure: float
+    arousal: float
+    dominance: float
+    
+class Persona, class Episodicmemory 등등
+    
+'''
---
+++ chatbot/infra/__init__.py
@@ -0,0 +1,5 @@
+'''
+infra
+실제 구현(llm, memory, 감정, persona 등등)
+
+'''
